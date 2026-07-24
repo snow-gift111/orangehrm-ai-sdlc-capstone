079a06a502a9674212210d16ebdb1e447b4ad683
snow-gift111 <snow_gift@epam.com>
2026-07-24 15:25:30 +0530

    feat(backend): add Spring Boot app entrypoint

---
+++ backend/src/main/java/com/elms/ElmsApplication.java
@@ -0,0 +1,11 @@
+package com.elms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigurate.SpringBootApplication;
+
+@SpringBootApplication
+public class ElmsApplication {
+  public static void main(String[] args) {
+    SpringApplication.run(ElmsApplication.class, args);
+  }
+}
