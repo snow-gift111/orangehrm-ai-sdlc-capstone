f38095a077385e5fcd37b9e255e322037251971c
snow-gift111 <snow_gift@epam.com>
2026-07-27 20:56:04 +0530

    Add authority constants

---
+++ src/main/java/com/orangehrm/lba/security/Authorities.java
@@ -0,0 +1,8 @@
+package com.orangehrm.lba.security;
+
+public final class Authorities {
+    private Authorities() {}
+
+    public static final String RULE_MANAGE = "LBA_RULE_MANAGE";
+    public static final String HISTORY_VIEW = "LBA_HISTORY_VIEW";
+}
