13c3ab95b150c05f052f241602e692914727ae82
snow-gift111 <snow_gift@epam.com>
2026-07-28 13:53:27 +0530

    Add permission constants for Leave Balance Alert RBAC

---
+++ backend/src/main/java/com/orangehrm/security/AppPermissions.java
@@ -0,0 +1,12 @@
+package com.orangehrm.security;
+
+public final class AppPermissions {
+
+    private AppPermissions() {
+    }
+
+    public static final String PERM_MANAGE_LEAVE_TYPES = "PERM_MANAGE_LEAVE_TYPES";
+    public static final String PERM_MANAGE_LEAVE_BALANCES = "PERM_MANAGE_LEAVE_BALANCES";
+    public static final String PERM_MANAGE_LEAVE_ALERT_RULES = "PERM_MANAGE_LEAVE_ALERT_RULES";
+    public static final String PERM_VIEW_ORG_LEAVE_ALERTS = "PERM_VIEW_ORG_LEAVE_ALERTS";
+}
