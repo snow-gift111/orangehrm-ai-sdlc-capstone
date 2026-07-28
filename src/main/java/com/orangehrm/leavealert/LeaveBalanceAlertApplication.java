27cf514b58323ac02547fcf4cf4872fc1a495857
snow-gift111 <snow_gift@epam.com>
2026-07-28 17:47:06 +0530

    Add Spring Boot application entry point

--- src/main/java/com/orangehrm/leavealert/LeaveBalanceAlertApplication.java
+++ src/main/java/com/orangehrm/leavealert/LeaveBalanceAlertApplication.java
@@ -1,13 +1,14 @@
 package com.orangehrm.leavealert;
 
+import com.orangehrm.leavealert.config.LeaveBalanceAlertProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
-@SpringBootApplication
 @EnableScheduling
-@ConfigurationPropertiesScan
+@SpringBootApplication
+@EnableConfigurationProperties(LeaveBalanceAlertProperties.class)
 public class LeaveBalanceAlertApplication {
 
     public static void main(String[] args) {
