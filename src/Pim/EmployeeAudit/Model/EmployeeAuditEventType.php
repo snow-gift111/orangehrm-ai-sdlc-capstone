ee2bf044877e962d1bac88458b6446c963fe9013
snow-gift111 <snow_gift@epam.com>
2026-08-02 15:09:39 +0530

    Add employee audit event type model

---
+++ src/Pim/EmployeeAudit/Model/EmployeeAuditEventType.php
@@ -0,0 +1,22 @@
+<?php
+
+declare(strict_types=1);
+
+namespace OrangeHRM\Pim\EmployeeAudit\Model;
+
+enum EmployeeAuditEventType: string
+{
+    case Create = 'Create';
+    case Update = 'Update';
+    case Delete = 'Delete';
+
+    public static function fromString(string $eventType): self
+    {
+        return match ($eventType) {
+            self::Create->value => self::Create,
+            self::Update->value => self::Update,
+            self::Delete->value => self::Delete,
+            default => throw new \InvalidArgumentException(sprintf('Unsupported employee audit event type "%s".', $eventType)),
+        };
+    }
+}
