9b8703d11f74214b0d385a58b01da4fe69afb0ff
snow-gift111 <snow_gift@epam.com>
2026-08-02 14:02:30 +0530

    Add Symfony application kernel

--- src/Kernel.php
+++ src/Kernel.php
@@ -5,23 +5,9 @@ declare(strict_types=1);
 namespace App;
 
 use Symfony\Bundle\FrameworkBundle\Kernel\MicroKernelTrait;
-use Symfony\Component\DependencyInjection\ContainerBuilder;
-use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
 use Symfony\Component\HttpKernel\Kernel as BaseKernel;
-use Symfony\Component\Routing\Loader\Configurator\RoutingConfigurator;
 
 final class Kernel extends BaseKernel
 {
     use MicroKernelTrait;
-
-    protected function configureContainer(ContainerConfigurator $container, ContainerBuilder $builder): void
-    {
-        $container->import('../config/packages/*.yaml');
-        $container->import('../config/services.yaml');
-    }
-
-    protected function configureRoutes(RoutingConfigurator $routes): void
-    {
-        $routes->import('../config/routes.yaml');
-    }
 }
